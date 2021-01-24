ed2bfd53da7605c915f813d5cca81b26a74b2123
soraya-elahi <71372912+soraya-elahi@users.noreply.github.com>
2021-01-24 22:04:44 +0100

    texture

--- sketch.js
+++ sketch.js
@@ -1,48 +1,47 @@
 function setup() {
 
-createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   background('#fae')
 }
 
 
-  function draw() {
+function draw() {
 
-  for(x = 80; x < width - 90; x += 40){
-    for(y =80; y < height - 90; y += 40){
+  for (x = 80; x < width - 90; x += 40) {
+    for (y = 80; y < height - 90; y += 40) {
 
 
-      if(random()<0.20)
-     {
-     fill('#fae');
-     } else {
+      if (random() < 0.20) {
+        fill('#fae');
+      } else {
         fill(214, 109, 145);
-     }
+      }
 
 
-     if (mouseX<300 && mouseX>300) {
-       fill(255, 204, 0);
-         };
+      if (mouseX < 300 && mouseX > 300) {
+        fill(255, 204, 0);
+      };
 
-         if (mouseX < random()*200){
-           fill('rgba');
-            }
+      if (mouseX < random() * 200) {
+        fill('rgba');
+      }
 
-         if (mouseX < random()*700){
-        fill(249,178,72);
-            }
+      if (mouseX < random() * 700) {
+        fill(249, 178, 72);
+      }
 
       rect(x, y, 30, 30);
     }
 
 
-  let myText = "move your mouse";
+    let myText = "move your mouse";
 
-  fill(color("black"));
-  textSize(25);
+    fill(color("black"));
+    textSize(25);
 
-  textFont("PT Serif");
-  textAlign(CENTER);
-  text(myText, width/2, 50);
+    textFont("PT Serif");
+    textAlign(CENTER);
+    text(myText, width / 2, 50);
   }
 
 }
